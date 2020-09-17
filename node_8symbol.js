ba9d3625f14982aa698e547d8f5a1f5afe08b7d1
fengjiao <lvfe@tcd.ie>
2020-09-17 09:00:59 +0800

    add sync

--- node_8symbol.js
+++ node_8symbol.js
@@ -31,3 +31,26 @@ console.log(g1.next()); //,有点像java iterator,{value:'welcone', done: false}
 console.log(g1.next()); //,有点像java iterator,{value:'to', done: false}
 console.log(g1.next()); //,有点像java iterator,{value:'js', done: true}
 console.log(g1.next()); //,有点像java iterator,{value:undefined, done: true}
+//loop for of,自动遍历generator, return的东西不会遍历
+for(let value of g1){
+    console.log(value);
+}
+// 不仅可以forof,也可以解构赋值
+let [a,b, c] = show();
+console.log(a, b, c); //welcome to
+let [a, ...b] = show();
+console.log(a,b); // welocme [to]
+// 还可以扩展运算符
+console.log(...g1); // welcome to
+// Array
+Array.from(g1); // [welcome, to]
+// generator配合axios api.github.com
+// 异步：不连续， 上一个操作没有执行完， 下一个操作昭阳开始
+// 同步， 连续， 上一个操作没有执行完， 下一个不能开始
+关于异步：
+1）回调函数
+2）事件监听
+3）帆布订阅
+4）promise对象
+co__ 每次需要generate麻烦， 有了co
+async
